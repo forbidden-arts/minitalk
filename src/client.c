b37f13d95a6a4e0e5677894a1b038b87f44dfad9
Dan Palmer <dj.palmer81@gmail.com>
2022-12-15 10:21:58 +0200

    for testing

--- src/client.c
+++ src/client.c
@@ -6,14 +6,54 @@
 /*   By: dpalmer <dpalmer@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 09:32:32 by dpalmer           #+#    #+#             */
-/*   Updated: 2022/12/13 09:45:12 by dpalmer          ###   ########.fr       */
+/*   Updated: 2022/12/15 10:13:33 by dpalmer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int	main(void)
+static void	ft_confirm(int signal)
 {
-	ft_printf("Balls\n");
+	if (signal)
+		ft_printf("Confirmation received.\n");
+}
+
+static void	ft_send_byte(int pid, char c)
+{
+	int	bit;
+
+	bit = 0;
+	while (bit < 8)
+	{
+		if ((c & (0b1 << bit)) != 0)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(SIG_DELAY);
+		bit++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+	int	i;
+
+	i = 0;
+	if (argc != 3)
+	{
+		ft_printf("Incorrect format.\n ./client {PID} {MESSAGE}\n");
+		return (1);
+	}
+	pid = ft_atoi(argv[1]);
+	while (argv[2][i])
+	{
+		signal(SIGUSR1, ft_confirm);
+		signal(SIGUSR2, ft_confirm);
+		ft_send_byte(pid, argv[2][i]);
+		i++;
+	}
+	ft_send_byte(pid, '\n');
+	ft_send_byte(pid, 0);
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,14 +6,51 @@
 /*   By: dpalmer <dpalmer@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 09:32:12 by dpalmer           #+#    #+#             */
-/*   Updated: 2022/12/13 09:45:09 by dpalmer          ###   ########.fr       */
+/*   Updated: 2022/12/15 10:21:43 by dpalmer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+static void	ft_sig_handler(int signal, siginfo_t *info, void *s)
+{
+	static int	bit;
+	static int	i;
+
+	(void)info;
+	(void)s;
+	if (signal == SIGUSR1)
+		i |= (0b1 << bit);
+	bit++;
+	if (bit == 8)
+	{
+		ft_printf("%c", i);
+		if (i == 0)
+			kill(info->si_pid, SIGUSR2);
+		bit = 0;
+		i = 0;
+	}
+}
+
+/* Notes for later: I can't think of any siguation where ./server needs to 
+process an argument, so I'm going to set it to void and see what happens. */
+
 int	main(void)
 {
-	ft_printf("sllaB\n");
+	int					pid;
+	struct sigaction	sa;
+
+	pid = getpid();
+	ft_printf("PID:\t%d\n", pid);
+	ft_printf("Waiting...\n");
+	sa.sa_sigaction = ft_sig_handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	while (1)
+	{
+		sigaction(SIGUSR1, &sa, NULL);
+		sigaction(SIGUSR2, &sa, NULL);
+		pause();
+	}
 	return (0);
 }
