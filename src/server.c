29c521b4bced66879fd3992abf9f1e68211724aa
Dan Palmer <dj.palmer81@gmail.com>
2022-12-15 10:51:15 +0200

    v1.0

--- src/server.c
+++ src/server.c
@@ -6,19 +6,20 @@
 /*   By: dpalmer <dpalmer@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 09:32:12 by dpalmer           #+#    #+#             */
-/*   Updated: 2022/12/15 10:21:43 by dpalmer          ###   ########.fr       */
+/*   Updated: 2022/12/15 10:46:55 by dpalmer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static void	ft_sig_handler(int signal, siginfo_t *info, void *s)
+// SigAction requires 3 args, even though we care about 2. Hence the (void)x .
+
+static void	ft_sig_handler(int signal, siginfo_t *info, void *x)
 {
 	static int	bit;
 	static int	i;
 
-	(void)info;
-	(void)s;
+	(void)x;
 	if (signal == SIGUSR1)
 		i |= (0b1 << bit);
 	bit++;
@@ -32,8 +33,14 @@ static void	ft_sig_handler(int signal, siginfo_t *info, void *s)
 	}
 }
 
-/* Notes for later: I can't think of any siguation where ./server needs to 
-process an argument, so I'm going to set it to void and see what happens. */
+/* Notes for later: I can't think of any situation where ./server needs to 
+process an argument, so I'm going to set it to void and see what happens. 
+--Addendum: seems to work just fine. Leaving as is.
+
+--Addendum #2: In order to handle a "return" signal, had to impliment 
+"sigaction", because we need to determine the sender's PID automatically.
+Not currently setting any of the other "flags" in the struct, that may
+or may not be adjusted in the future if there are protection issues.	*/
 
 int	main(void)
 {
@@ -44,8 +51,6 @@ int	main(void)
 	ft_printf("PID:\t%d\n", pid);
 	ft_printf("Waiting...\n");
 	sa.sa_sigaction = ft_sig_handler;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = 0;
 	while (1)
 	{
 		sigaction(SIGUSR1, &sa, NULL);
